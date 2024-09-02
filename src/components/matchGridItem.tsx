c14e88c80266ba2580390af6817b7edab6c8ad8f
Jonas Svensson <jonas.m.svensson@gmail.com>
2024-09-02 13:06:16 +0200

    Add all match information

--- src/components/matchGridItem.tsx
+++ src/components/matchGridItem.tsx
@@ -26,6 +26,59 @@ async function getHandsByGameId(id: string): Promise<any> {
     connection.release();
   }
 }
+async function getTeamIdToNameNoAlias() {
+  const connection = await Connection.getInstance().getConnection();
+  try {
+    const [result] = await connection.query(
+      "SELECT COALESCE(GROUP_CONCAT(Players.NAME order by Players.NAME separator '+')) as NAME, Teams.TEAM_ID from Teams \
+   INNER JOIN \
+   Players \
+   ON \
+   Players.PLAYER_ID = Teams.PLAYER_ID \
+   GROUP BY Teams.TEAM_ID"
+    );
+
+    // Convert the result to an object with the team_id as the key and the name as the value
+    const teamIdToName = result.reduce((acc: any, row: any) => {
+      acc[row.TEAM_ID] = row.NAME;
+      return acc;
+    }, {});
+
+    return teamIdToName;
+  } finally {
+    connection.release();
+  }
+}
+
+async function getTeamIdToName() {
+  const connection = await Connection.getInstance().getConnection();
+  try {
+    const [result] = await connection.query(
+      "SELECT COALESCE(MAX(TeamAttributes.`VALUE`), GROUP_CONCAT(Players.NAME order by Players.NAME separator '+')) as NAME, Teams.TEAM_ID from Teams \
+ LEFT OUTER JOIN \
+ TeamAttributes \
+ ON \
+ TeamAttributes.`TEAM_ID` = Teams.TEAM_ID \
+ INNER JOIN \
+ Players \
+ ON \
+ Players.PLAYER_ID = Teams.PLAYER_ID \
+ WHERE \
+  TeamAttributes.ATTRIBUTE IS NULL OR TeamAttributes.ATTRIBUTE = 'alias' \
+ GROUP BY Teams.TEAM_ID"
+    );
+
+    // Convert the result to an object with the team_id as the key and the name as the value
+    const teamIdToName = result.reduce((acc: any, row: any) => {
+      acc[row.TEAM_ID] = row.NAME;
+      return acc;
+    }, {});
+
+    return teamIdToName;
+  } finally {
+    connection.release();
+  }
+}
 
 export default async function MatchGridItem({ id }: { id: string }) {
   const match = await getMatchById(id);
@@ -48,6 +101,9 @@ export default async function MatchGridItem({ id }: { id: string }) {
     new Date(lastRound)
   )}`;
 
+  const teamIdToName = await getTeamIdToNameNoAlias();
+  const getTeamName = (teamId: string) => teamIdToName[teamId];
+
   return (
     <div
       style={{ border: "1px solid #ccc", padding: "10px", borderRadius: "5px" }}
@@ -58,7 +114,11 @@ export default async function MatchGridItem({ id }: { id: string }) {
       <Link href={`/match/${id}`}>
         #{match.GAME_IDX}. {name}
       </Link>
-      <p>{numberOfRounds} omgångar,</p>
+      <p>
+        {numberOfRounds} omgångar, {getTeamName(match.TEAM_ID_1)},{" "}
+        {getTeamName(match.TEAM_ID_2)}, {getTeamName(match.TEAM_ID_3)},{" "}
+        {getTeamName(match.TEAM_ID_4)}
+      </p>
     </div>
   );
 }
