898d7e2720a1ffbd3cf617c79ddb0f559a71bb33
Jonas Svensson <jonas.m.svensson@gmail.com>
2024-10-19 09:06:35 +0200

    Update header css

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -6,12 +6,17 @@ export default function Header() {
   return (
     <AppBar
       position="static"
-      style={{ background: "linear-gradient(to right, #FF3366, #FF6B6B)" }}
+      className="header-bar"
     >
       <Toolbar>
         <Box sx={{ display: "flex", alignItems: "center", flexGrow: 1 }}>
-          <Link href="/" passHref>
-            <Box sx={{ display: "flex", alignItems: "center", cursor: "pointer" }}>
+          <Link
+            href="/"
+            passHref
+          >
+            <Box
+              sx={{ display: "flex", alignItems: "center", cursor: "pointer" }}
+            >
               <Image
                 src="/mahjong_tiles.png"
                 alt="Mahjong Tiles"
@@ -20,52 +25,31 @@ export default function Header() {
               />
               <Typography
                 variant="h6"
-                style={{
-                  marginLeft: "15px",
-                  color: "white",
-                  fontWeight: "300",
-                  textAlign: "left",
-                  letterSpacing: "1px"
-                }}
+                className="header-text"
               >
-                <span style={{ fontWeight: "700" }}>Mahjong</span> Master System 4.0
+                <span className="bold-header">Mahjong</span> Master System 4.0
               </Typography>
             </Box>
           </Link>
         </Box>
-        <Box sx={{ display: "flex", alignItems: "center", justifyContent: "flex-end", flexGrow: 1 }}>
+        <Box className="header-links">
           <Link
             href="/"
             passHref
           >
-            <Typography
-              variant="body1"
-              style={{ color: "white", marginRight: "20px", cursor: "pointer" }}
-            >
-              matcher
-            </Typography>
+            <Typography variant="body1">matcher</Typography>
           </Link>
           <Link
             href="/statistics"
             passHref
           >
-            <Typography
-              variant="body1"
-              style={{ color: "white", marginRight: "20px", cursor: "pointer" }}
-            >
-              statistik
-            </Typography>
+            <Typography variant="body1">statistik</Typography>
           </Link>
           <Link
             href="/scoreboard"
             passHref
           >
-            <Typography
-              variant="body1"
-              style={{ color: "white", marginRight: "20px", cursor: "pointer" }}
-            >
-              poängtabell
-            </Typography>
+            <Typography variant="body1">poängtabell</Typography>
           </Link>
         </Box>
       </Toolbar>
