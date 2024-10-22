dc3585ba2fd7c651ad8a4f524a6febee760f33e9
Jonas Svensson <jonas.m.svensson@gmail.com>
2024-10-22 19:32:50 +0200

    Update layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,12 +22,8 @@ export default function RootLayout({
         <Container>
           <Box
             sx={{
-              maxWidth: { xs: "100%", sm: "90%", md: "80%", lg: "70%", xl: "60%" },
-              minWidth: { xs: "100%", sm: "90%", md: "80%", lg: "70%", xl: "60%" },
-              paddingTop: "20px",
               margin: "0 auto",
               backgroundColor: "white",
-              padding: "20px",
             }}
           >
             <Header />
