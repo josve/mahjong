63e523e65c0e31630baff2090f50771317b93376
Jonas Svensson <jonas.m.svensson@gmail.com>
2024-11-09 09:14:06 +0100

    Improve matches and grid

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,53 +1,48 @@
 import MatchGridItem from "@/components/matchGridItem";
 import TotalStatisticsRow from "@/components/totalStatisticsRow";
 import Link from "next/link";
-import { Grid } from "@mui/material";
+import { Box } from "@mui/material";
+import Grid from '@mui/material/Grid2';
 import { getAllMatches} from "@/lib/fetchMatches";
 
 export const revalidate = 60;
 
 export default async function Home() {
-  const matches = await getAllMatches();
+    const matches = await getAllMatches();
 
-  return (
-    <>
-      <div
-        style={{
-          display: "flex",
-          justifyContent: "space-between",
-          alignItems: "center",
-          marginBottom: "20px",
-        }}
-      >
-        <h1>
-          Matcher
-        </h1>
-        <Link
-          href="/match/new"
-          passHref
-        >
-          <button className="button create-match-button">Skapa ny match</button>
-        </Link>
-      </div>
-      <TotalStatisticsRow />
-      <Grid
-        container
-        spacing={3}
-        sx={{ marginTop: "20px" }}
-      >
-        {" "}
-        {matches.map((match: { GAME_ID: string }) => (
-          <Grid
-            item
-            xs={12}
-            sm={6}
-            key={match.GAME_ID}
-          >
-            {" "}
-            <MatchGridItem id={match.GAME_ID} />
-          </Grid>
-        ))}
-      </Grid>
-    </>
-  );
+    return (
+        <>
+            <Box className="matches-header">
+                <h1>
+                    Matcher
+                </h1>
+                <Box sx={{display: {xs: "none", ms: "block"}}}
+                >
+                    <Link
+                        href="/match/new"
+                        passHref
+                    >
+                        <button className="button create-match-button">Skapa ny match</button>
+                    </Link>
+                </Box>
+            </Box>
+            <TotalStatisticsRow/>
+            <Grid
+                container
+                spacing={3}
+                sx={{marginTop: "20px"}}
+            >
+                {" "}
+                {matches.map((match: { GAME_ID: string }) => (
+                    <Grid
+                        size={{ xs: 12, sm: 6}}
+                        key={match.GAME_ID}
+                    >
+                        {" "}
+                        <MatchGridItem id={match.GAME_ID}/>
+                    </Grid>
+                ))}
+            </Grid>
+        </>
+    );
 }
