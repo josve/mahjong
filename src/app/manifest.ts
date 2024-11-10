d8bd14b9a9e51f9201b554e3c4d497d2a9083811
Jonas Svensson <jonas.m.svensson@gmail.com>
2024-11-10 08:12:08 +0100

    update

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -6,7 +6,6 @@ export default function manifest(): MetadataRoute.Manifest {
         short_name: 'Master System',
         description: 'System for keeping track of mahjong scores',
         start_url: '/',
-        orientation: 'landscape',
         display: 'standalone',
         background_color: '#fff',
         theme_color: 'rgb(229,70,70)',
